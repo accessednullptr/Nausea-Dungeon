7ad41d13c5a516fac30f102d1e6fa84e6f8377fa
nullptr <accessednullptr@gmail.com>
2022-06-12 11:27:21 -0400

    Fixed GameplayDebugger being included as a dependency on all targets.

--- Source/NauseaDungeon/NauseaDungeon.Build.cs
+++ Source/NauseaDungeon/NauseaDungeon.Build.cs
@@ -14,11 +14,12 @@ public class NauseaDungeon : ModuleRules
         PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "NetCore", "PhysicsCore", "InputCore" });
         PublicDependencyModuleNames.AddRange(new string[] { "AIModule", "NavigationSystem" });
         PublicDependencyModuleNames.AddRange(new string[] { "SlateCore", "Slate", "UMG" });
-        PublicDependencyModuleNames.AddRange(new string[] { "GameplayAbilities", "GameplayTags", "GameplayTasks", "GameplayDebugger", "EngineSettings", "AnimationBudgetAllocator" });
+        PublicDependencyModuleNames.AddRange(new string[] { "GameplayAbilities", "GameplayTags", "GameplayTasks", "EngineSettings", "AnimationBudgetAllocator" });
 
         if (Target.Type == TargetType.Editor)
         {
             PublicDependencyModuleNames.AddRange(new string[] { "RenderCore", "RHI", "UnrealEd" });
+            PublicDependencyModuleNames.AddRange(new string[] { "GameplayDebugger" });
         }
     }
 }
